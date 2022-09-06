5fb959c523c0fef2d5556a0eb83b535f2032cdb4
crashmax-dev <crashmax.dev@gmail.com>
2022-09-06 12:53:23 +0000

    Deploying to gh-pages from @ crashmax-dev/vite-userscript-template@a2d7aef7c1400edaaaeba978710010793f72a3db 🚀

---
+++ vite-userscript-template.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        vite-userscript-template
+// @version     0.0.0
+// @homepage    https://crashmax-dev.github.io/vite-userscript-template/
+// @match       https://example.com
+// @grant       GM_addStyle
+// @updateURL   https://crashmax-dev.github.io/vite-userscript-template/vite-userscript-template.meta.js
+// @downloadURL https://crashmax-dev.github.io/vite-userscript-template/vite-userscript-template.user.js
+// ==/UserScript==
