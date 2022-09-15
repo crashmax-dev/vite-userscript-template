ec4c71c25bf6e88faa97e528fe20a955a9e1a2ea
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-09-16 01:55:19 +0800

    jsx reatom v2

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,41 @@
 import { mount } from 'redom-jsx'
+import type { RedomComponent, RedomEl } from 'redom-jsx'
+import { createAtom } from '@reatom/core'
 import './style.scss'
 
-function App() {
-  return (
-    <div className="card">
-      <h1 className="title">Hello World</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, ipsa.
-      </p>
-      <a
-        href="https://google.com"
-        target="_blank"
-      >
-        Link
-      </a>
+const counterAtom = createAtom(
+  {
+    increase: () => {},
+    decrease: () => {},
+    reset: () => 0
+  },
+  ({ onAction, schedule }, state = 0) => {
+    onAction('increase', () => state++)
+    onAction('decrease', () => state--)
+    onAction('reset', (value) => (state = value))
+
+    schedule(() => console.log(`Counter: ${state}`))
+
+    return state
+  }
+)
+
+class Counter implements RedomComponent {
+  el: RedomEl
+  h1: HTMLHeadingElement
+
+  constructor() {
+    // prettier-ignore
+    <div this="el" className="card">
+      <h1 this="h1"></h1>
+      <button onclick={() => counterAtom.decrease.dispatch()}>decrease</button>
+      <button onclick={() => counterAtom.reset.dispatch()}>reset</button>
+      <button onclick={() => counterAtom.increase.dispatch()}>increase</button>
     </div>
-  )
+  }
 }
 
-mount(document.body, <App />)
+const counter = new Counter()
+counterAtom.subscribe((value) => (counter.h1.textContent = `${value}`))
+
+mount(document.body, counter)
