8ea42391fe3c39bffc8f686ea4e078e62b45f017
crashmax-dev <crashmax.dev@gmail.com>
2023-07-06 10:12:07 +0000

    Deploying to gh-pages from @ crashmax-dev/vite-userscript-template@e29c91a719f3829b5558d2fa8b5dfe22bf0312fc 🚀

--- vite-userscript-template.user.js
+++ vite-userscript-template.user.js
@@ -11,13 +11,4 @@
 // @downloadURL https://crashmax-dev.github.io/vite-userscript-template/vite-userscript-template.user.js
 // ==/UserScript==
 
-(function(){"use strict";function n(a,t,...r){const e=document.createElement(a);return t instanceof Node?e.append(t):typeof t=="string"?e.append(o(t)):Array.isArray(t)?e.append(...t):(Object.assign(e,t),Object.assign(e.style,t?.style)),e.append(...r),e}function o(a){return document.createTextNode(a)}const d="",c=n("div",{className:"card"},n("h1",{className:"title"},"Hello World"),n("p","Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, ipsa."),n("a",{href:"https://google.com",target:"_blank"},"Link"));document.body.appendChild(c),GM_addStyle(`.card {  color: #fff;
-  background-color: #333;
-}
-.card .title {
-  text-decoration: underline;
-}
-.card a {
-  color: #637fff;
-}
-`)})();
+(function(){"use strict";function n(a,t,...r){const e=document.createElement(a);return t instanceof Node?e.append(t):typeof t=="string"?e.append(o(t)):Array.isArray(t)?e.append(...t):(Object.assign(e,t),Object.assign(e.style,t?.style)),e.append(...r),e}function o(a){return document.createTextNode(a)}const d="",c=n("div",{className:"card"},n("h1",{className:"title"},"Hello World"),n("p","Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, ipsa."),n("a",{href:"https://google.com",target:"_blank"},"Link"));document.body.appendChild(c),GM_addStyle(".card{color:#fff;background-color:#333}.card .title{text-decoration:underline}.card a{color:#637fff}")})();
